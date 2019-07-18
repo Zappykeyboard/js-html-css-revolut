7a73a05dc87edbb5f9d5d3429300c1c614432d62
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 16:22:46 +0200

    reso più specifico il selettore alla riga 5

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   
   $(".with-drop").on("click", function(){
     console.log("click");
-    $("*").find(".drop-down").removeClass("active");
+    $("#nav-right").find(".drop-down").removeClass("active");
     $(this).find(".drop-down").toggleClass("active");
   });
 
