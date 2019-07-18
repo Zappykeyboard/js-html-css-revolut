338d24d00c1c4ad3f16be6ff37f783a15dc93d78
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 16:32:35 +0200

    rimosso console.log

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   
   $(".with-drop").on("click", function(){
-    console.log("click");
     $("#nav-right").find(".drop-down").removeClass("active");
     $(this).find(".drop-down").toggleClass("active");
   });
