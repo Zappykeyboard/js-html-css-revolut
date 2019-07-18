98f4f5b78d6182f14fd0c2e09a50a738c14c2e28
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 16:21:58 +0200

    al click, ora gli altri dropdown si chiudono

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   
   $(".with-drop").on("click", function(){
     console.log("click");
+    $("*").find(".drop-down").removeClass("active");
     $(this).find(".drop-down").toggleClass("active");
   });
 
