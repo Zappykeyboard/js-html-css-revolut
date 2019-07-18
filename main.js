cc77fb21c776a7f1af675c1491cd57fcbff49bdd
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 16:55:16 +0200

    aggiunti commenti

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   
   $(".with-drop").on("click", function(){
+    //se ci sono altri dropdown aperti nella nav, li chiudo
     $("#nav-right").find(".drop-down").removeClass("active");
+
+    //trovo il dropdown figlio e lo apro
     $(this).find(".drop-down").toggleClass("active");
   });
 
