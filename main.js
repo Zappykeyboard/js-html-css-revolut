92d2bd14ab1a050703b5a03547451a93b95573ad
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 18:23:36 +0200

    aggiunta nuova logica

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
   $(".with-drop").on("click", function(){
     
     //se ci sono altri dropdown aperti nella nav, li chiudo
-    //$(".drop-down").not(this).removeClass("active");
-    
+    $(".with-drop").not(this).find(".drop-down").removeClass("active");
+
+
     //trovo il dropdown figlio e lo apro
     $(this).find(".drop-down").toggleClass("active");
 
